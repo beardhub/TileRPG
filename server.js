305e5fc532fcf6944ad54c4a141776d9ade812a7
Zack Ervin <zjervin@gmail.com>
2017-09-03 01:21:03 -0400

    blah

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
+var io = require('socket.io', { rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling'] })(http);//.listen(8080);
 var port = process.env.PORT || 8080;
 app.use(express.static(__dirname));
 var worlddata = {
